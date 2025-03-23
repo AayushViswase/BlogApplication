807ed4e6cb02c34dbb8c9aa8764c76d28bbdadd9
AayushViswase <aayushviswase10@gmail.com>
2025-03-23 17:52:04 +0530

    Refactor main entry point to simplify rendering and focus on About component

--- Frontend/Blog/src/main.tsx
+++ Frontend/Blog/src/main.tsx
@@ -1,23 +1,7 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import About from "./About.tsx";
-import Login from "./Login.tsx";
-import SignUp from "./Signup.tsx";
-import Navbar from "./NavBar.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./About";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" Component={() => <About />} />
-        <Route path="/login" Component={() => <Login />} />
-        <Route path="/signup" Component={() => <SignUp />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+function Main() {
+  return <About />;
+}
+
+export default Main;
